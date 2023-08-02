828d1d07afc7fcbe293f6c7bdbcb608b511808b3
Victor <mrsklyarov.v@gmail.com>
2023-08-02 14:23:01 +0300

    Redundant code removed

--- app/src/main/java/com/zeroillusion/demonetworkapp/data/remote/PixabayApi.kt
+++ app/src/main/java/com/zeroillusion/demonetworkapp/data/remote/PixabayApi.kt
@@ -7,12 +7,6 @@ import retrofit2.http.Query
 
 interface PixabayApi {
 
-    //@GET("/api/")
-    //suspend fun searchForImage(
-    //    @Query("q") searchQuery: String,
-    //    @Query("key") apiKey: String = BuildConfig.API_KEY
-    //) : Response<ImageResponse>
-
     @GET("/api/")
     suspend fun getAllImages(
         @Query("key") apiKey: String = BuildConfig.API_KEY
